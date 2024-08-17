e1e68fec6107dae4c047e7824ce69594d5fc3bdc
chunned <hnolan@protonmail.com>
2024-08-16 21:50:26 -0400

    Complete restructure

--- databass.py
+++ databass.py
@@ -1,351 +1,43 @@
-import flask
-import api
-import db
-from uuid import uuid4
+from flask import Flask
+import routes
+import util
 import os
+from models import app_db
 from dotenv import load_dotenv
-from flask_paginate import Pagination, get_page_parameter
+from uuid import uuid4
 
-# Application initialization
 load_dotenv()
-DB_FILENAME = os.getenv("DB_FILENAME")
-db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), DB_FILENAME)
-app = flask.Flask(__name__)
-app.secret_key = uuid4().hex
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
-app.static_folder = 'static'
-db.db.init_app(app)
 
 
-@app.before_request
-def create_tables():
-    db.db.create_all()
+class Config:
+    SECRET_KEY = uuid4().hex
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.environ.get('DB_FILENAME')
+    DISCOGS_KEY = os.environ.get('DISCOGS_KEY')
+    DISCOGS_SECRET = os.environ.get('DISCOGS_SECRET')
+    TIMEZONE = os.environ.get('TIMEZONE')
+    STATIC_FOLDER = 'static'
+    DEBUG = True
 
 
-def download_all_missing_images():
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    routes.register_routes(app)
+    util.register_filters(app)
+
+    app_db.init_app(app)
+    print('DB initialized')
     with app.app_context():
-        print('INFO: Fetching all missing images')
-        data = db.get_all_id_and_img()
-        for rel in data["releases"]:
-            print(f'INFO: Release ID: {rel[0]}, Image URL: {rel[1]}')
-            if not img_exists(
-                item_id=rel[0],
-                item_type='release',
-                img_url=rel[1]
-            ):
-                print('INFO: Fetching image...')
-                api.download_image(
-                    item_type='release',
-                    item_id=rel[0],
-                    img_url=rel[1]
-                )
-            else:
-                print('INFO: Image already exists')
-        for lab in data["labels"]:
-            print(f'INFO: Label ID: {lab[0]}, Image URL: {lab[1]}')
-            if not img_exists(
-                item_id=lab[0],
-                item_type='label',
-                img_url=lab[1]
-            ):
-                print('INFO: Fetching image...')
-                api.download_image(
-                    item_type='label',
-                    item_id=lab[0],
-                    img_url=lab[1]
-                )
-            else:
-                print('INFO: Image already exists')
-        for art in data["artists"]:
-            print(f'INFO: Artist ID: {art[0]}, Image URL: {art[1]}')
-            if not img_exists(
-                    item_id=art[0],
-                    item_type='artist',
-                    img_url=art[1]
-            ):
-                print('INFO: Fetching image...')
-                api.download_image(
-                    item_type='artist',
-                    item_id=art[0],
-                    img_url=art[1]
-                )
-            else:
-                print('INFO: Image already exists')
-
-
-@app.route("/", methods=['GET'])
-@app.route('/home', methods=['GET'])
-def home():
-    data = db.get_homepage_data()
-    home_stats = db.get_stats()
-    #download_all_missing_images()
-    page = flask.request.args.get(
-        get_page_parameter(),
-        type=int,
-        default=1
-    )
-    pagination = data.paginate(
-        page=page,
-        per_page=5,
-        error_out=True
-    )
-
-    paged_data = pagination.items
-
-    flask_pagination = Pagination(
-        page=page,
-        total=pagination.total,
-        search=False,
-        record_name='latest_releases'
-    )
-
-    return flask.render_template(
-        'index.html',
-        data=paged_data,
-        stats=home_stats,
-        pagination=flask_pagination,
-        active_page='home'
-    )
-
-
-@app.route("/new")
-def new():
-    actions = ["search"]
-    return flask.render_template("new.html", actions=actions, active_page='new')
-
-
-@app.route("/search", methods=["POST"])
-def search():
-    search_data = flask.request.get_json()
-    search_release = search_data["release"]
-    search_artist = search_data["artist"]
-    search_label = search_data["label"]
-
-    data = api.pick_release(search_release, search_artist, search_label)
-
-    return flask.render_template("search/static.html", data=data)
-
-
-@app.route("/submit", methods=["POST"])
-def submit():
-    data = flask.request.form.to_dict()
-    print(f'INFO: Request to submit release: {data}')
-    release_id = data["release_id"]
-    artist_id = data["artist_id"]
-    label_id = data["label_id"]
-    year = data["release_year"]
-    genre = data["genre"]
-    rating = data["rating"]
-    tags = data["tags"]
-    print(f"INFO: Fetching release info from API")
-    release_data = api.get_release_data(release_id, year, genre, rating)
-    print(f"INFO: API Response data: {release_data}")
-    release_data["tags"] = tags
-    if label_id:
-        print(f"INFO: Checking if label with ID {label_id} exists in database")
-        # check if label exists in database already, avoid some API calls
-        # db.exists() returns an array; 1st element is True/False, 2nd is the entity ID
-        label_exists = db.exists(mbid=label_id, item_type='label')
-        if not label_exists[0]:
-            print(f'INFO: No existing label found; inserting a new label')
-            print(f'INFO: Fetching label info from API')
-            # not in db already, get data and insert it
-            label_data = api.get_label_data(label_id)
-            print(f'INFO: API Response data: {label_data}')
-            label_id = db.insert_label(label_data)
-            # download image
-            label_image_url = label_data["image"]
-            print(f'INFO: Downloading label image from {label_image_url}')
-            label_image_path = api.download_image(
-                item_type='label',
-                item_id=label_id,
-                img_url=label_image_url
-            )
-            print(f'INFO: Label image downloaded to {label_image_path}')
-            release_data["label_id"] = label_id
-        else:
-            print(f'INFO: Label already exists in database with ID {label_exists[1]}')
-            release_data["label_id"] = label_exists[1]
-
-    # check if artist exists in db already
-    # db.exists() returns an array; 1st element is True/False, 2nd is the entity ID
-    print(f"INFO: Checking if artist with ID {artist_id} exists in database")
-    artist_exists = db.exists(
-        mbid=artist_id,
-        item_type='artist'
-    )
-    if not artist_exists[0]:
-        # not in db
-        print(f"INFO: No existing artist found; inserting a new artist")
-        print(f'INFO: Fetching artist info from API')
-        artist_data = api.get_artist_data(artist_id)
-        print(f'INFO: API Response data: {artist_data}')
-        artist_id = db.insert_artist(artist_data)
-        # download image
-        artist_image_url = artist_data["image"]
-        print(f'INFO: Downloading artist image from {artist_image_url}')
-        artist_image_path = api.download_image(
-            item_type='artist',
-            item_id=artist_id,
-            img_url=artist_image_url
-        )
-        print(f'INFO: Artist image downloaded to {artist_image_path}')
-        release_data["artist_id"] = artist_id
-    else:
-        print('INFO: Artist already exists in database.')
-        release_data["artist_id"] = artist_exists[1]
-    print(f"INFO: Inserting release into database")
-    db_id = db.insert_release(release_data)    # db_id = primary key of release
-    print(f'INFO: Release insertion successful with ID: {db_id}')
-    # Download image after inserting release into db
-    release_image_url = release_data["image"]
-    print(f'INFO: Downloading release image from {release_image_url}')
-    release_image_path = api.download_image(
-        item_type='release',
-        item_id=db_id,
-        img_url=release_image_url)
-    print(f'INFO: Image saved to {release_image_path}')
-    return flask.redirect("/", code=302)
-
-
-@app.route('/releases', methods=["GET"])
-def releases():
-    genres = db.distinct_entries(table=db.Release, column='genre')
-    countries = db.distinct_entries(table=db.Release, column='country')
-    all_labels = db.distinct_entries(table=db.Label, column='name')
-    all_artists = db.distinct_entries(table=db.Artist, column='name')
-    all_releases = db.distinct_entries(table=db.Release, column='name')
-    data = {
-        "genres": genres,
-        "countries": countries,
-        "labels": all_labels,
-        "releases": all_releases,
-        "artists": all_artists
-    }
-    return flask.render_template(
-        'releases.html',
-        data=data,
-        active_page='releases'
-    )
-
-
-@app.route('/artists', methods=["GET"])
-def artists():
-    countries = db.distinct_entries(table=db.Artist, column='country')
-    data = {"countries": countries}
-    return flask.render_template('artists.html', data=data, active_page='artists')
-
-
-@app.route('/labels', methods=["GET"])
-def labels():
-    countries = db.distinct_entries(table=db.Label, column='country')
-    types = db.distinct_entries(table=db.Label, column='type')
-    data = {"countries": countries, "types": types}
-    return flask.render_template('labels.html', data=data, active_page='labels')
-
-
-@app.route('/release/<string:release_id>', methods=['GET'])
-def release(release_id):
-    # Displays all info related to a particular release
-    release_data = db.get_item(item_type='release', item_id=release_id)
-    return flask.render_template('release.html', data=release_data)
-
-
-@app.route('/artist/<string:artist_id>', methods=['GET'])
-def artist(artist_id):
-    # Displays all info related to a particular artist
-    artist_data = db.get_item(item_type='artist', item_id=artist_id)
-    return flask.render_template('artist.html', data=artist_data)
-
-
-@app.route('/label/<string:label_id>', methods=['GET'])
-def label(label_id):
-    label_data = db.get_item(item_type='label', item_id=label_id)
-    return flask.render_template('label.html', data=label_data)
-
-
-@app.route('/charts', methods=['GET'])
-def charts():
-    # Not implemented
-    # con = db.create_connection('music.db')
-    # cur = db.create_cursor(con)
-    #
-    # query = "SELECT rating FROM release"
-    # cur.execute(query)
-    # data = cur.fetchall()
-    #
-    # data = [n[0] for n in data]
-    # return flask.render_template('charts.html', data=data)
-    return flask.redirect('/', 302)
-
-
-@app.route('/edit/<string:release_id>', methods=['GET'])
-def edit(release_id):
-    data = db.get_item('release', release_id)
-    return flask.render_template('edit.html', data=data)
-
-
-@app.route('/edit-release', methods=['POST'])
-def edit_release():
-    edit_data = flask.request.form.to_dict()
-    db.update_release(edit_data)
-
-    return flask.redirect('/', 302)
-
-
-@app.route('/delete', methods=['POST', 'GET'])
-def delete():
-    deletion_id = flask.request.get_json()['id']
-    db.delete_item('release', deletion_id)
-    return flask.redirect('/', 302)
-
-
-@app.route('/add-review', methods=['POST'])
-def add_review():
-    review_data = flask.request.form.to_dict()
-    db.add_review(review_data)
-    return flask.redirect('/', 302)
-
-
-@app.route('/stats', methods=['GET'])
-def stats():
-    statistics = db.get_stats()
-    return flask.render_template('stats.html', data=statistics, active_page='stats')
-
-
-@app.route('/dynamic_search', methods=['POST'])
-def dynamic_search():
-    form_data = flask.request.get_json()
-    search_results = db.dynamic_search(form_data)
-    return flask.render_template('search/dynamic.html', data=search_results)
-
-
-@app.route('/submit-manual', methods=['POST'])
-def submit_manual():
-    data = flask.request.form.to_dict()
-    db.submit_manual(data)
-    return flask.redirect('/', 302)
-
-
-@app.template_filter('img_exists')
-def img_exists(item_id, item_type, img_url):
-    extension = api.get_image_type_from_url(img_url)
-    print(f'INFO: Checking if image exists for {item_type} {item_id}{extension}')
-    path = os.path.join(app.static_folder, 'img', item_type, f'{item_id}{extension}')
-    print(f'INFO: Checking path: {path}')
-    result = os.path.isfile(path)
-    print(f'INFO: {result}')
-    if result:
-        url = flask.url_for('static', filename=f'img/{item_type}/{item_id}{extension}')
-        print(f'INFO: Returning {url}')
-        return url
-    else:
-        return result
-
-
-def main():
-    app.run(host='0.0.0.0', debug=True, port=8080)
+        app_db.create_all()
+        print('Tables created')
+    return app
 
 
 if __name__ == '__main__':
-    main()
+    app = create_app()
+
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=8080
+    )
---
+++ models.py
@@ -0,0 +1,64 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import Optional
+
+
+class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+app_db = SQLAlchemy(
+    model_class=Base
+)
+
+
+class Release(app_db.Model):
+    __tablename__ = "release"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    mbid: Mapped[Optional[str]] = mapped_column(String, unique=True)
+    artist_id: Mapped[int] = mapped_column(ForeignKey("artist.id"))
+    label_id: Mapped[int] = mapped_column(ForeignKey("label.id"))
+    name: Mapped[str] = mapped_column(String())
+    release_year: Mapped[int] = mapped_column(Integer())
+    runtime: Mapped[int] = mapped_column(Integer())
+    rating: Mapped[int] = mapped_column(Integer())
+    listen_date: Mapped[str] = mapped_column(String())
+    track_count: Mapped[int] = mapped_column(Integer())
+    country: Mapped[str] = mapped_column(String())
+    genre: Mapped[str] = mapped_column(String())
+    tags: Mapped[Optional[str]] = mapped_column(String())
+    image: Mapped[Optional[str]] = mapped_column(String())
+    review: Mapped[Optional[str]] = mapped_column(String())
+
+    def __init__(self, mbid: Optional[str] = None, artist_id: int = 0, label_id: int = 0, **kwargs):
+        self.mbid = mbid
+        self.artist_id = artist_id
+        self.label_id = label_id
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class ArtistOrLabel(app_db.Model):
+    # Artist and Label tables are both built from this prototype
+    __abstract__ = True
+    id: Mapped[int] = mapped_column(primary_key=True)
+    mbid: Mapped[Optional[str]] = mapped_column(String(), unique=True)
+    name: Mapped[str] = mapped_column(String())
+    country: Mapped[Optional[str]] = mapped_column(String())
+    type: Mapped[Optional[str]] = mapped_column(String())
+    begin_date: Mapped[Optional[str]] = mapped_column(String())
+    end_date: Mapped[Optional[str]] = mapped_column(String())
+    image: Mapped[Optional[str]] = mapped_column(String())
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class Label(ArtistOrLabel):
+    __tablename__ = "label"
+
+
+class Artist(ArtistOrLabel):
+    __tablename__ = "artist"
