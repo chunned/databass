4632339dfcd32a3cee1ef3e90d4423eeeb41aa28
chunned <hnolan@protonmail.com>
2024-08-24 01:06:57 -0400

    Add port environment variables

--- databass.py
+++ databass.py
@@ -11,7 +11,8 @@ db_name = os.environ.get('DB_NAME')
 db_user = os.environ.get('PG_USER')
 db_password = os.environ.get('PG_PASSWORD')
 db_hostname = os.environ.get('PG_HOSTNAME')
-db_connection_string = f'postgresql://{db_user}:{db_password}@{db_hostname}/{db_name}'
+db_port = os.environ.get('PG_PORT')
+db_connection_string = f'postgresql://{db_user}:{db_password}@{db_hostname}:{db_port}/{db_name}'
 
 
 class Config:
