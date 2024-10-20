57923262a312ce9b78efcfe84bb54b615d4a35ec
chunned <hnolan@protonmail.com>
2024-10-19 23:45:32 -0400

    Tests for db.util.get_model()

--- src/tests/db/test_util.py
+++ src/tests/db/test_util.py
@@ -2,6 +2,32 @@ import pytest
 from databass.db.util import *
 
 
+class TestGetModel:
+    # Tests for get_model()
+    def test_get_model_success(self, mocker):
+        class MockModel:
+            pass
+        mock_globals = mocker.patch("databass.db.util.globals", return_value={"Model": MockModel})
+        result = get_model("Model")
+        assert result == MockModel
+        mock_globals.assert_called_once()
+
+    @pytest.mark.parametrize(
+        "model",
+        [1, [1], {"1": 1}, 1.0]
+    )
+    def test_get_model_fail_invalid_input(self, model):
+        with pytest.raises(ValueError, match="model_name must be a string"):
+            get_model(model)
+
+    def test_get_model_fail_model_not_found(self, mocker):
+        class MockModel:
+            pass
+        mock_globals = mocker.patch("databass.db.util.globals", return_value={"Model": MockModel})
+        with pytest.raises(NameError, match="No model with the name"):
+            get_model("TestModel")
+
+
 class TestMeanAvgAndCount:
     # Tests for mean_avg_and_count()
     def test_mean_avg_and_count_successful_return(self):
@@ -57,14 +83,19 @@ class TestBayesianAvg:
         
 class TestGetAllStats:
     # Tests for get_all_stats()
-    def test_get_all_stats_successful_error_handling(self):
-        """
-        Test for successful handling of errored stats function result
-        """
-
-    def test_get_all_stats_successful_return(self):
+    def test_get_all_stats_success(self):
         """
         Test for correct handling of successfully returned stats data
         """
 
+    def test_get_all_stats_fail(self):
+        """
+        Test for successful handling of errored stats function result
+        """
 
+
+
+class TestHandleSubmitData:
+    # Tests for handle_submit_data()
+    def test_handle_submit_test_success(self):
+        pass
